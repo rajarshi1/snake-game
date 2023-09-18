33cfa8e423db5e937edbe62b89efbc2487fbb9b1
Rajarshi Ghoshal <rajarshi@192.168.1.2>
2023-09-18 17:14:29 +0530

    move snake

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useRef, useEffect  } from "react";
-import { intervalHook } from "./intervalHook";
+import { useIntervalHook } from "./intervalHook";
 import {
    SCALE,
    SPEED,
@@ -15,17 +15,18 @@ function App() {
   const canvasRef = useRef();
   const [snake, setSnake] = useState(START_AT);
   const [rat, setRat] = useState(RAT_POP);
-  const [dir, setDir] = useState(0,-1);
-  const [level, setLevel] = useState(null);
+  const [dir, setDir] = useState([0,-1]);
+  const [level, setLevel] = useState(500);
   const [gameOver, setGameOver] = useState(false);
+
   function gameInit() {
     
   }
   function endGame(){
 
   }
-  function snakeControll(){
-
+  function snakeControll({ keycode }){
+    return setDir(CONTROLS[keycode]);
   }
   function ratInit(){
 
@@ -37,17 +38,23 @@ function App() {
 
   }
   function loop(){
-
+    const snakeCopy = JSON.parse(JSON.stringify(snake));
+    const newSnakeHead = [snakeCopy[0][0] + dir[0], snakeCopy[0][1] + dir[1]];
+    snakeCopy.unshift(newSnakeHead);
+    snakeCopy.pop();
+    setSnake(snakeCopy);
   }
   useEffect(() => {
     const context = canvasRef.current.getContext('2d');
     context.setTransform(SCALE, 0, 0, SCALE, 0, 0);
-    context.clearRect(0, 0, BOARD_SIZE[0], BOARD_SIZE[1]);
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
     context.fillStyle = 'pink';
     snake.forEach(([x,y]) => context.fillRect(x, y, 1, 1));
     context.fillStyle = 'lightblue';
     context.fillRect(rat[0], rat[1], 1, 1); 
-  }, [snake, rat, gameOver])
+  }, [snake, rat, gameOver]);
+
+  useIntervalHook(() => loop(), level )
 
   return (
     <div role="button" tabIndex="0" onKeyDown={e => snakeControll(e)}>
